c0e3e1076cf59fa4d57d9913224f97b8f62b20fb
Cole <letchro@gmail.com>
2022-10-25 11:36:41 -0500

    init

---
+++ src/main/java/net/yorksolutions/remakejavastore/RemakeJavaStoreApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.remakejavastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemakeJavaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemakeJavaStoreApplication.class, args);
+    }
+
+}
